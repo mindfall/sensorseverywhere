2702c3d404ea2e76f24d4c97bd397bc6c1d29633
Wade Mansell <wade_mansell@hotmail.com>
2014-11-15 16:31:29 +1100

    uglified

--- server/config/config.js
+++ server/config/config.js
@@ -6,10 +6,10 @@ module.exports = {
 		db: 'mongodb://localhost/wildFire',
 		rootPath: rootPath,
 		port: process.env.PORT || 3033
-	}
-	/*production: {
-		db: 'mongodb://miriad:multivision@ds061558.mongolab.com:61558/multivision',
+	},
+	production: {
+		db: 'mongodb://localhost/wildFire',
 		rootPath: rootPath,
 		port: process.env.PORT || 80
-	}*/
+	}
 }
